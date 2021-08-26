a7675b59d96df63410a35b37af238aa860d9a133
Jorge Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2021-08-26 19:02:45 -0400

    13 desafios Algoritmos

---
+++ Dojo_Assignments/Web_Fundamentals/Javascript/13_desafios/get_1_to_255.js
@@ -0,0 +1,19 @@
+
+/*=============================================
+=            Imprime numeros del 1 al 255     =
+=============================================*/
+
+function imprime1to255(){
+     suma=0;
+     for(var i=1; i<=255;i++){
+          console.log(i);
+          suma=suma+i;
+
+     }
+     return suma
+}
+var y = imprime1to255();
+console.log(y);
+
+/*=====  End of Section comment block  ======*/
+
--- Dojo_Assignments/Web_Fundamentals/Javascript/suma2numeros.js
+++
@@ -1,7 +0,0 @@
-function suma(num1,num2) {     
-     let suma = 0;
-     suma = num1 + num2;
-     return suma
-}
-let y = suma(3,5);
-console.log(y);
