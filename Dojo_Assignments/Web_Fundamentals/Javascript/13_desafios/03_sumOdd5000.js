3aea926674bbfb32c24d3c52ba696c39f6118f08
Jorge Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2021-08-28 13:29:05 -0400

    13 desafios Algoritmos

---
+++ Dojo_Assignments/Web_Fundamentals/Javascript/13_desafios/03_sumOdd5000.js
@@ -0,0 +1,18 @@
+
+/*=============================================
+=            13 Desafíos de Algoritmos            =
+=============================================*/
+
+function sum_odd_5000() {
+     var sum = 0;
+     //your code here 
+     for(var i=1;i<=5000;i++){
+         if(i%2!==0){
+             sum+=i;
+         }
+     }
+     return sum; 
+ }
+
+/*=====  Imprime la suma de impares entre el 1 el el 5000  ======*/
+
---
+++ Dojo_Assignments/Web_Fundamentals/Javascript/13_desafios/04_iterateAnArray.js
@@ -0,0 +1,16 @@
+
+/*=============================================
+=            13 Desafíos de Algoritmos               =
+=============================================*/
+
+function iterArr(arr) {
+     //your code here
+     var sum =0;
+     for(var i=0;i<arr.length;i++){
+         sum+=arr[i];
+     }
+     return sum; 
+ }
+
+/*=====  Funcion retorna la suma de todos los numeros de un arreglo  ======*/
+
