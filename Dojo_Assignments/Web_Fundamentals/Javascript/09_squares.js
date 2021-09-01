1f300308d8f695a6fecda2d4c4b3baa640ea2dda
Jorge Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2021-09-01 01:07:22 -0400

    Devolver el valor de cada indice del array al cuadrado

---
+++ Dojo_Assignments/Web_Fundamentals/Javascript/09_squares.js
@@ -0,0 +1,15 @@
+
+/*=============================================
+=            13 Desafíos de Algoritmos        =
+=============================================*/
+
+function squareVal(arr) {
+     //your code here 
+     for(var i=0;i<arr.length;i++){
+         arr[i]=arr[i]*arr[i]
+     }
+     return arr; 
+ }
+
+/*=====  Devolver el cada valor del array al cuadrado ======*/
+
