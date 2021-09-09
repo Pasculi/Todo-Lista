f2a1aef24aad2456ff6ff92da46313de80289cd6
Jorge Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2021-09-09 18:52:07 -0300

    Rest / Spread

--- Dojo_Assignments/MERN/arrowFunction/fatArrow.js
+++ Dojo_Assignments/MERN/arrowFunction/fatArrow.js
@@ -8,3 +8,8 @@ const sumArrow =(a,b)=> a+b;
 console.log(sumArrow(5,4));
 
 
+var sayHello = function(name) {
+     console.log('Hello ' + name);
+   };
+
+const sayHello =(name)=>console.log("Hello" + name)
