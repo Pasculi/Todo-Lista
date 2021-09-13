3c98a7291257598eaa78b3dbebc219ca139edc38
Jorge Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2021-09-13 19:24:35 -0300

    Ninja

---
+++ Dojo_Assignments/MERN/ninja/ninja.js
@@ -0,0 +1,24 @@
+class Ninja{
+     constructor(nombre, salud, velocidad, fuerza){
+          this.nombre = nombre;
+          this.salud = salud;
+          this.velocidad = 3
+          this.fuerza = 3
+     }
+     sayName(){
+          console.log(`El nombre del ninja es: ${this.nombre}`);
+          
+     }
+     showStats(){          
+          console.log(`El nombre del ninja ${this.nombre} - Fuerza: ${this.fuerza} - Velocidad: ${this.velocidad} - Salud: ${this.salud}`);
+     }
+     drinkSake (){
+          this.salud+=10;
+     } 
+}
+
+const ninja1 = new Ninja("Sekigahara", 100);
+
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
