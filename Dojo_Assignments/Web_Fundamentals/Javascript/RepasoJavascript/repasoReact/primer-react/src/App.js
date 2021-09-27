bc1e6c36ceb9af03d72fb17c3ae7248cde566b02
Jorge Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2021-09-27 09:41:06 -0300

    Repaso React, componente funcional

--- Dojo_Assignments/Web_Fundamentals/Javascript/RepasoJavascript/repasoReact/primer-react/src/App.js
+++ Dojo_Assignments/Web_Fundamentals/Javascript/RepasoJavascript/repasoReact/primer-react/src/App.js
@@ -1,14 +1,19 @@
+import React, { useState } from 'react';
 import './App.css';
 import Home from './views/Home';
+import Login from './views/Login';
 
 
 
 function App() {
+  const msg ='Soy codigo javascript :)';
+  const [age, setAge] = useState(1);
   return (
     <div className="App">
       <h1>Hola Ninjas!!!</h1>
-      <Home />
-      <Home />
+      <Home message ={msg} />
+      <Login age={age} setAge = {setAge}/>
+      
     </div>
   );
 }
---
+++ Dojo_Assignments/Web_Fundamentals/Javascript/RepasoJavascript/repasoReact/primer-react/src/views/Login.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+
+const Login = (props) => {
+
+    const {age, setAge} = props;
+
+    return(
+        <div>
+            <h1>Soy un componente funcional y soy mas cool!!</h1>
+            <h3>{':-)'}</h3>
+            <h1>{age}</h1>
+            <button onClick={() => setAge(age + 1)}>Sumar edad</button>
+
+        </div>
+
+    )
+}
+
+export default Login;
