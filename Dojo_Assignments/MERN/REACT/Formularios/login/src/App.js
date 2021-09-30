6ac3b886a26d81683aff0b76d6a595f7692bf69d
Jorge Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2021-09-30 11:35:56 -0300

    login app core

--- Dojo_Assignments/MERN/REACT/Formularios/login/src/App.js
+++ Dojo_Assignments/MERN/REACT/Formularios/login/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import Login from './views/login'
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [nombre, setNombre] = nombre ('');
+    const [apellido, setApellido] = setState ('');
+    const [email, setEmail] = setState ('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login nombre={nombre} apellido={apellido} email={email}/>
     </div>
   );
 }
