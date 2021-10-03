485e2d916d57b23fbb27a897882fd9da04d36435
Jorge Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2021-10-02 22:50:39 -0300

    Repaso

--- Dojo_Assignments/MERN/REACT/Formularios/login/src/App.js
+++ Dojo_Assignments/MERN/REACT/Formularios/login/src/App.js
@@ -1,14 +1,13 @@
-import Login from './views/login'
 import './App.css';
-import React, { useState } from 'react';
+import Login from './views/Login';
+
+
 
 function App() {
-  const [nombre, setNombre] = nombre ('');
-    const [apellido, setApellido] = setState ('');
-    const [email, setEmail] = setState ('');
   return (
     <div className="App">
-      <Login nombre={nombre} apellido={apellido} email={email}/>
+      <h1>GTD Telsur</h1>
+      <Login msg='Yo soy un props desde la App.js'/>
     </div>
   );
 }
