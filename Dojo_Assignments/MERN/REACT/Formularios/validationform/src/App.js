47e34a71d1afb0cd732a0a182f2bf5921224c4cb
Jorge Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2021-10-07 20:08:58 -0300

    Formulario Hooks

--- Dojo_Assignments/MERN/REACT/Formularios/validationform/src/App.js
+++ Dojo_Assignments/MERN/REACT/Formularios/validationform/src/App.js
@@ -1,10 +1,20 @@
 import './App.scss';
+import React, { useState } from 'react';
 import Registro from './views/Registro';
 
 function App() {
+
+const [user, setUser] = useState({
+  firstName:'',
+  lastName:'',
+  email:'',
+  password:'',
+  confirmPassword:''
+}
+);
   return (
     <div className="App">
-      <Registro />
+      <Registro userApp={user} setUserApp={setUser}/>
     </div>
   );
 }
--- Dojo_Assignments/MERN/REACT/Formularios/validationform/src/views/Registro.js
+++ Dojo_Assignments/MERN/REACT/Formularios/validationform/src/views/Registro.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 
-const Registro = () => {
+const Registro = (props) => { //const Registro = (userApp, setUserApp) => {
+
+    const {userApp, setUserApp} = props;
+    const {firstName, lastName,email, password, confirmPassword} = userApp;
+
+    const handleChange = (e) => {
+        setUserApp({...userApp, [e.target.className]: e.target.value});
+    }
+
+
     return (
         <div className='card-container'>
             <h1>Registro de usuario</h1>
             <Form>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label htmlFor="firstName" className="etiqueta">Nombre:</Form.Label>
-                    <Form.Control id="firstName" className="input" type="text" placeholder="Ingrese su nombre" />
+                    <Form.Control onChange={handleChange} name="firstName" className="input" type="text" placeholder="Ingrese su nombre" />
                     <Form.Text className="text-muted">
                         <p className="danger">El campo debe tener al menos 2 caracteres.</p>
                     </Form.Text>
@@ -16,7 +25,7 @@ const Registro = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label htmlFor="lastName" className="etiqueta">Apellido:</Form.Label>
-                    <Form.Control id="lastName" className="input" type="text" placeholder="Ingrese su apellido" />
+                    <Form.Control onChange={handleChange} name="lastName" id="lastName" className="input" type="text" placeholder="Ingrese su apellido" />
                     <Form.Text className="text-muted">
                         <p className="danger">El campo debe tener al menos 2 caracteres.</p>
                     </Form.Text>
@@ -24,7 +33,7 @@ const Registro = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label htmlFor="email" className="etiqueta">Email:</Form.Label>
-                    <Form.Control id="email" className="input" type="email" placeholder="Ingrese un email" />
+                    <Form.Control onChange={handleChange} name="email" id="email" className="input" type="email" placeholder="Ingrese un email" />
                     <Form.Text className="text-muted">
                         <p className="danger">El campo debe tener al menos 5 caracteres.</p>
                     </Form.Text>
@@ -32,7 +41,7 @@ const Registro = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label htmlFor="password" className="etiqueta">Contraseña:</Form.Label>
-                    <Form.Control id="password" className="input" type="password" placeholder="Ingrese una contraseña" />
+                    <Form.Control onChange={handleChange} name="password" id="password" className="input" type="password" placeholder="Ingrese una contraseña" />
                     <Form.Text className="text-muted">
                         <p className="danger">El campo debe tener al menos 8 caracteres.</p>
                     </Form.Text>
@@ -40,7 +49,7 @@ const Registro = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label htmlFor="confirmPassword" className="etiqueta">Confirme contraseña:</Form.Label>
-                    <Form.Control id="confirmPassword" className="input" type="password" placeholder="Confirmar contraseña" />
+                    <Form.Control onChange={handleChange} name="confirmPassword" id="confirmPassword" className="input" type="password" placeholder="Confirmar contraseña" />
                     <Form.Text className="text-muted">
                         <p className="danger">Las contraseñas no coiciden.</p>
                     </Form.Text>
