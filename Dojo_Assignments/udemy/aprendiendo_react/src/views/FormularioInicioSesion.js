0663c7bbd55fdbad62e98096f085c09ac3f94f50
Jorge Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2021-10-11 01:28:02 -0300

    Curso React Udemy Validaciones

--- Dojo_Assignments/udemy/aprendiendo_react/src/views/FormularioInicioSesion.js
+++ Dojo_Assignments/udemy/aprendiendo_react/src/views/FormularioInicioSesion.js
@@ -1,51 +1,91 @@
 import React, { useState } from 'react';
+import './../App.css';
 
 
 const FormularioInicioSesion = () => {
-    const [usuario, cambiaUsuario] = useState('');
-    const [password, cambiaPassword] = useState('');
+    const [formUser, setFromUser] = useState({
+        nombre: '',
+        apellido: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    });
+    const [errMsg, setErrMsg] = useState('');
 
-    const onChange = (evento) => {
-        if (evento.target.name === 'usuario') {
-            cambiaUsuario(evento.target.value);
-
-        } else if (evento.target.name === 'password') {
-            cambiaPassword(evento.target.value);
-
-        }
+    const handleChange = (e) => {
+        setFromUser({ ...formUser, [e.target.name]: e.target.value });
 
     }
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        if (Object.values(formUser).includes('')) {
+            setErrMsg('Debe llenar todos los campos');
+        } else {
+            setErrMsg('');
 
-    return (
-        <div>
+        }
+    };
 
-            <form action="">
-                <p>Usuario: {usuario} </p>
-                <p>Contraseña: {password} </p>
-                <br />
-                <br />
-                <br />
-                <label htmlFor="user">Usuario:</label>
+return (
+    <div>
+
+        <form onSubmit={handleOnSubmit}>
+            <p>Usuario: {formUser.nombre} </p>
+            <p>Contraseña: {formUser.apellido} </p>
+            <p>Usuario: {formUser.email} </p>
+            <p>Contraseña: {formUser.password} </p>
+            <p>Confirmar contraseña: {formUser.confirmPassword} </p>
+            <br />
+            <br />
+            <br />
+            <div>
+                <label htmlFor="nombre">Usuario:</label>
                 <input
-                    name="usuario"
-                    id="user"
+                    name="nombre"
+                    id="nombre"
                     type="text"
-                    value={usuario}
-                    onChange={onChange}
-                /><br />
-                <label htmlFor="pass">Contraseña:</label>
-                <input
-                    name="password"
-                    id="pass"
-                    type="password"
-                    value={password}
-                    onChange={onChange}
-                /><br />
-                <button type="submit">Iniciar sesión</button>
+                    value={formUser.nombre}
+                    onChange={handleChange}
+                /> <br />
+            </div>
+            <label htmlFor="apellido">Apellido:</label>
+            <input
+                name="apellido"
+                id="apellido"
+                type="text"
+                value={formUser.apellido}
+                onChange={handleChange}
+            /><br />
+            <label htmlFor="email">Email:</label>
+            <input
+                name="email"
+                id="email"
+                type="text"
+                value={formUser.email}
+                onChange={handleChange}
+            /><br />
+            <label htmlFor="password">Contraseña:</label>
+            <input
+                name="password"
+                id="password"
+                type="password"
+                value={formUser.password}
+                onChange={handleChange}
+            /><br />
+            <label htmlFor="confirmPassword">Confirmar Contraseña:</label>
+            <input
+                name="confirmPassword"
+                id="confirmPassword"
+                type="password"
+                value={formUser.confirmPassword}
+                onChange={handleChange}
+            /><br />
+            <button type="submit">Iniciar sesión</button>
 
-            </form>
-        </div>
-    )
+        </form>
+        <p className='error'>{errMsg}</p>
+    </div>
+)
 }
 
 export default FormularioInicioSesion
