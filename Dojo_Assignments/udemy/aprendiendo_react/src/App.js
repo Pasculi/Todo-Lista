75f2f424b46de77523c760eaf0c15652e013551e
Jorge Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2021-10-12 02:31:14 -0300

    Validar formulario

--- Dojo_Assignments/udemy/aprendiendo_react/src/App.js
+++ Dojo_Assignments/udemy/aprendiendo_react/src/App.js
@@ -1,33 +1,12 @@
-import React, { useState } from 'react';
-import Contador from './views/Contador';
-import FormularioInicioSesion from './views/FormularioInicioSesion';
-//import Arrays from './views/Arrays';
-//import { Titulos } from './views/Titulos';
-import User from './views/Usuario';
-
+import './App.css';
+import FormularioUsuario from './views/FormularioUsuario';
 
 const App = () => {
-  const [session, cambiaSession] = useState(true);
 
   return (
     <>
-      {session === true ?
-        <div>
-          <Contador incremento={24} decremento={8}/>
-          <User usuario="Jorge" />
-          <button onClick={() => cambiaSession(false)}>Cerrar Sesion</button>
-        </div>
-        :
-        <div>
-          <h2>No has iniciado sesion</h2>
-          <FormularioInicioSesion />
-          {/*<button onClick={() => cambiaSession(true)}>Iniciar Sesion</button>*/}
-        </div>
-      }
+      <FormularioUsuario />
     </>
-
   );
 }
-
-
 export default App;
