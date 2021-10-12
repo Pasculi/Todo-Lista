10e3efc859a307dd9b2b40830c9b15afc682de27
Jorge Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2021-10-12 02:41:47 -0300

    Validación de formulario

---
+++ Dojo_Assignments/MERN/REACT/Formularios/validationform/src/views/MensajeErrores.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const MensajeErrores = () => {
+    
+    const [errMsg, setErrMsg]
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default MensajeErrores
---
+++ Dojo_Assignments/MERN/REACT/Formularios/validationform/src/views/Usuario.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const User = ({usuario}) => {
+    return (
+        <div>
+            <h2>Bienvenido(a) {usuario} </h2>
+        </div>
+    )
+}
+
+export default User
