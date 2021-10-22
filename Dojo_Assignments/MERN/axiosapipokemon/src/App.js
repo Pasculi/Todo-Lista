7665afe54c7a101029f82813417a33557db0a6e6
Jorge Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2021-10-22 18:16:56 -0300

    Axios_Pokemon

---
+++ Dojo_Assignments/MERN/axiosapipokemon/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/moleculas/header/Header.jsx';
+
+function App() {
+  return (
+
+    <div className="App">
+      <Header />
+
+    </div>
+
+  );
+}
+
+export default App;
---
+++ Dojo_Assignments/MERN/axiosapipokemon/src/components/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Yo soy Home</h1>
+            
+        </div>
+    )
+}
+
+export default Home
