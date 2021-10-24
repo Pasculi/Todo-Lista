a0c7f0bc6af90aa3f486c642e5a09c64217030bb
Jorge Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2021-10-23 23:35:41 -0300

    Repaso2

---
+++ Dojo_Assignments/MERN/mi-primera-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HolaMundo from './views/HolaMundo';
+
+function App() {
+  return (
+    <div className="App">
+      <HolaMundo />
+    </div>
+  );
+}
+
+export default App;
